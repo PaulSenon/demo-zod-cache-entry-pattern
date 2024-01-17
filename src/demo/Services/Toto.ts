cc63dd84fa1d167fcccde99bede2e615adc13126
Paul Senon <paul.senon@gmail.com>
2024-01-17 18:38:34 +0000

    demo-1

---
+++ src/demo/Services/Toto.ts
@@ -0,0 +1,33 @@
+const SDK_URL = "https://sdktoto.com/sdk.js";
+
+type TotoSDK = {
+  somethingReallyInteresting: () => void;
+};
+
+declare global {
+  interface Window {
+    totoSdk?: TotoSDK;
+  }
+}
+
+export default class Toto {
+  private sdk?: any;
+
+  async init() {
+    this.sdk = await this.loadSDK();
+  }
+
+  private loadSDK(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      const script = document.createElement("script");
+      script.src = SDK_URL;
+      script.onload = () => resolve(window.totoSdk);
+      script.onerror = () => reject();
+      document.head.appendChild(script);
+    });
+  }
+
+  doSomething() {
+    this.sdk?.somethingReallyInteresting();
+  }
+}
